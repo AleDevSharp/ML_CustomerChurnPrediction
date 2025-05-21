4f219d64b9b30141bc843359eb9128a44eec5bc4
DiscoHub12 <discordo12di@gmail.com>
2025-05-21 11:10:22 +0200

    + init project

---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+------------------------------------------------------------------------
+File : app.py
+Description: Main project
+Date creation: 21-05-2025
+Project : soup-server
+Author: Alessio Giacché, Matteo Brachetta, Lorenzo
+Copyright: Copyright (c) 2024 Alessio Giacché <ale.giacc.dev@gmail.com>
+License : MIT
+------------------------------------------------------------------------
+"""
+
+if __name__ == '__main__':
+    print('Hello World')
